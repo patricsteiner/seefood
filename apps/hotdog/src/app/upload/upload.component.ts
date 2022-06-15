e9bc172b85378b0130b8f832b8bbd65d9dae6d07
yhe112114 <hycmedia@icloud.com>
2022-06-15 18:40:54 +0200

    added image preview before upload

--- apps/hotdog/src/app/upload/upload.component.ts
+++ apps/hotdog/src/app/upload/upload.component.ts
@@ -11,8 +11,22 @@ export class UploadComponent {
 
   file?: File
 
+  filePreviewSource?: string;
+
   setFile(fileInputElement: any) {
     this.file = fileInputElement.files[0]
+    const fileReader = new FileReader();
+
+    fileReader.onload = () => {
+      this.filePreviewSource = fileReader.result as string;
+    }
+
+    if (this.file) {
+      const blob = this.file as Blob;
+      fileReader.readAsDataURL(blob);
+    } else {
+      this.filePreviewSource = undefined;
+    }
   }
 
   uploadFile() {
